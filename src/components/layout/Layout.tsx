33f10065f48dcf4c092c6a581d4325fcc7142912
jwinton@veroskills.com <jwinton@veroskills.com>
2023-01-10 15:47:23 -0800

    Initial commit

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,23 @@
+import type { PropsWithChildren } from "react";
+import { Header } from "../header/Header";
+import { clsx } from "clsx";
+import { Inter } from "@next/font/google";
+import { Footer } from "../footer/Footer";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const Layout = ({ children }: PropsWithChildren) => {
+  return (
+    <>
+      <main
+        className={clsx("flex min-h-screen flex-col px-9", inter.className)}
+      >
+        <Header />
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
+};
