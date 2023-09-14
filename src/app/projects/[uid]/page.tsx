c17894160cf0b49fb45a3ac431791c0e04d85c3b
Jesse Winton <jrandallwinton@gmail.com>
2023-09-13 23:20:55 -0400

    update

--- src/app/projects/[uid]/page.tsx
+++ src/app/projects/[uid]/page.tsx
@@ -7,7 +7,7 @@ import { ScrollObserver } from "components/scroll-observer/ScrollObserver";
 import { ProjectNav } from "components/project-nav/ProjectNav";
 import Link from "next/link";
 import { ImageGallery } from "components/image-gallery/ImageGallery";
-import { getBlurUrl, getImageUrl, getPreviewUrl } from "utils/get-url";
+import { getBlurUrl, getImageUrl } from "utils/get-url";
 import type { Metadata } from "next";
 import { AdditionalVimeoPlayer } from "components/vimeo-player/AdditionalVimeoPlayer";
 
