f19c3965e3b5a8237d1be428f50eed0be598c593
Jesse Winton <jesse@cosmos.so>
2025-11-09 18:05:33 -0500

    Update Navigation.tsx

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -3,7 +3,6 @@
 import { asLink } from '@prismicio/helpers'
 import Link from 'next/link'
 import type { NavigationProps } from 'types/prismic'
-import { HoverLine } from 'components/hover-line/HoverLine'
 
 export const Navigation = ({ navigation }: { navigation: NavigationProps }) => {
   return (
@@ -15,10 +14,9 @@ export const Navigation = ({ navigation }: { navigation: NavigationProps }) => {
               item.items.length ? '' : (asLink(item.primary.link) as string)
             }
             key={item.primary.label}
-            className="tracking-loose group relative flex items-center justify-center text-sm font-normal uppercase text-neutral-600"
+            className="group relative flex items-center justify-center font-normal text-neutral-500 text-sm uppercase tracking-loose hover:text-neutral-900"
           >
             {item.primary.label}
-            <HoverLine />
           </Link>
         )
       })}
